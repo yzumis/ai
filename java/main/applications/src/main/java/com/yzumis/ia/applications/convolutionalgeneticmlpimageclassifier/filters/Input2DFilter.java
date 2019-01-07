01f13a310201d296773834c64328c1c538011852
jllodra <jllodra@hotelbeds.com>
2019-01-07 17:58:57 +0100

    Rename package

--- java/main/applications/src/main/java/com/yzumis/ia/applications/convolutionalgeneticmlpimageclassifier/filters/Input2DFilter.java
+++
@@ -1,9 +0,0 @@
-package com.yzumis.ia.applications.convolutionalgeneticmlpimageclassifier.filters;
-
-import com.yzumis.ia.applications.convolutionalgeneticmlpimageclassifier.input.Input2D;
-
-public interface Input2DFilter {
-
-    Input2D apply(final Input2D input2D);
-
-}
--- java/main/applications/src/main/java/com/yzumis/ia/applications/geneticmlpcar/object/Paintable.java
+++
@@ -1,9 +0,0 @@
-package com.yzumis.ia.applications.geneticmlpcar.object;
-
-import java.awt.*;
-
-public interface Paintable {
-
-    void paint(final Graphics graphics);
-
-}
